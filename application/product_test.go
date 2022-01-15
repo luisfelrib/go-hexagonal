6dbfbd2d00201f491e7f1d3efcc42a505b202d1d
Luis Felipe <luisfelrib@gmail.com>
2022-01-15 17:54:17 -0300

    :bug: fix application import

--- application/product_test.go
+++ application/product_test.go
@@ -1,13 +1,13 @@
 package application_test
 
 import (
-	"application"
+	"github.com/luisfelrib/go-hexagonal/application"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestProductEnable(t *testing.T) {
+func TestProduct_Enable(t *testing.T) {
 	product := application.Product{}
 	product.Name = "Hello"
 	product.Status = application.DISABLED
